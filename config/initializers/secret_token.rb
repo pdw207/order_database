8a7e69424d88b70ccb64e7ba1655051217ff26c4
pdw207 <pdw207@gmail.com>
2013-12-22 12:46:35 -0500

    Token moved to fix security issue

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-MujeresCambiaMail::Application.config.secret_key_base = '1b4959b425b6c2a65be1d7592ae714a0d6e3dca0e79ffda440c36729f02617ef693af2f588e2f29014418cb70c1172fbc79e5852751c2812bf4c5cb5366b030d'
+MujeresCambiaMail::Application.config.secret_key_base = ENV["TOKEN"]
